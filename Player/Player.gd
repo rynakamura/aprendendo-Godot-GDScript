c1fc8ec103a57aad7adb9db8d8838a75a6fc8fb3
Renato Nakamura <renato.yukio30@hotmail.com>
2022-06-05 20:27:02 -0300

    aula 13

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,7 +1,7 @@
 extends KinematicBody2D
-const ACCELERATION = 600
-const FRICTION = 600
-const MAX_SPEED = 150
+const ACCELERATION = 500
+const FRICTION = 500
+const MAX_SPEED = 100
 
 enum {
 	MOVE,
@@ -11,20 +11,24 @@ enum {
 
 var state = MOVE
 var velocity = Vector2.ZERO
+var roll_vector = Vector2.LEFT
 
 onready var animationPlayer = $AnimationPlayer
 onready var animationTree = $AnimationTree
 onready var animationState = animationTree.get('parameters/playback')
+onready var swordHitBox = $HitboxPivot/SwordHitBox
 
-#func _ready(): esse equivale ao onready acima
-#	animationPlayer = $AnimationPlayer
+
+func _ready(): 
+	animationTree.active = true
+	swordHitBox.knockback_vector = Vector2.LEFT
 
 func _physics_process(delta):
 	match state:
 		MOVE:
 			move_state(delta)
 		ROLL:
-			pass
+			roll_state(delta)
 		ATTACK:
 			atk_state(delta)
 
@@ -35,20 +39,25 @@ func move_state(delta):
 	result = result.normalized()
 	if result != Vector2.ZERO:
 #		animationPlayer.play("runRight")
+		roll_vector = result
+		swordHitBox.knockback_vector = result
 		animationTree.set('parameters/idle/blend_position',result) #o parametro é o indicado ao apoiar o mouse no atributo a esquerda
 		animationTree.set('parameters/run/blend_position',result)
 		animationTree.set('parameters/atk/blend_position',result)
+		animationTree.set('parameters/roll/blend_position',result)
 		animationState.travel("run")
 		velocity = velocity.move_toward(result * MAX_SPEED,ACCELERATION * delta)
+		move()
 #		velocity += result * ACCELERATION * delta
 #		velocity = velocity.clamped(MAX_SPEED) #clamped é limitador de velocidade
 	else:
 #		animationPlayer.play("idleRight")
 		animationState.travel('idle')
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
-	velocity = move_and_slide(velocity)
 	if Input.is_action_just_pressed("attack"):
 		state = ATTACK
+	if Input.is_action_just_pressed("roll"):
+		state = ROLL
 
 func atk_state(delta):
 	velocity = Vector2.ZERO
@@ -56,3 +65,15 @@ func atk_state(delta):
 	
 func on_atk_end():
 	state = MOVE
+	
+func roll_state(delta):
+	velocity = (roll_vector * MAX_SPEED )/1.2
+	animationState.travel('roll')
+	move()
+	
+func on_roll_end():
+	state = MOVE
+	
+func move():
+	velocity = move_and_slide(velocity)
+	
